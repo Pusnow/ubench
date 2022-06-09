00d57a3c50fe3e2048bd257a5d6953c029eb84bd
Wonsup Yoon <pusnow@kaist.ac.kr>
2022-06-09 11:59:51 +0900

    [python-lib] use class style interface

--- lib/python/lib.cpp
+++ lib/python/lib.cpp
@@ -8,24 +8,17 @@
 
 namespace py = pybind11;
 
-#define ADD_DEFS_ALL(func_name)                                                \
-    py::class_<ubench_##func_name##_t>(m, STRINGIFY(func_name) "_t")           \
-        .def(py::init<>());                                                    \
-    m.def(STRINGIFY(func_name) "_init", &ubench_##func_name##_init);           \
-    m.def(STRINGIFY(func_name) "_reset", &ubench_##func_name##_reset);         \
-    m.def(STRINGIFY(func_name) "_add", &ubench_##func_name##_add);             \
-    m.def(STRINGIFY(func_name) "_print", &ubench_##func_name##_print);         \
-    m.def(STRINGIFY(func_name) "_add_print", &ubench_##func_name##_add_print); \
-    m.def(STRINGIFY(func_name) "_add_print_reset",                             \
-          &ubench_##func_name##_add_print_reset);                              \
-    m.def(STRINGIFY(func_name) "_dump", &ubench_##func_name##_dump);           \
-    m.def(STRINGIFY(func_name) "_add_dump", &ubench_##func_name##_add_dump);   \
-    m.def(STRINGIFY(func_name) "_add_dump_reset",                              \
-          &ubench_##func_name##_add_dump_reset);
-
-#define ADD_DEFS_ALL_DOUBLE(func_name)                                         \
-    m.def(STRINGIFY(func_name) "_print_double",                                \
-          &ubench_##func_name##_print_double);
+#define ADD_DEFS_ALL(func_name, ...)                                           \
+    py::class_<ubench::func_name>(m, STRINGIFY(func_name))                     \
+        .def(py::init<__VA_ARGS__>())                                          \
+        .def("reset", &ubench::func_name::reset)                               \
+        .def("add", &ubench::func_name::add)                                   \
+        .def("print", &ubench::func_name::print)                               \
+        .def("add_print", &ubench::func_name::add_print)                       \
+        .def("add_print_reset", &ubench::func_name::add_print_reset)           \
+        .def("dump", &ubench::func_name::dump)                                 \
+        .def("add_dump", &ubench::func_name::add_dump)                         \
+        .def("add_dump_reset", &ubench::func_name::add_dump_reset);
 
 PYBIND11_MODULE(ubench, m) {
     m.doc() = R"pbdoc(
@@ -39,11 +32,12 @@ PYBIND11_MODULE(ubench, m) {
 
     m.def("ubench_get_cycles", &ubench_get_cycles);
     ADD_DEFS_ALL(stat);
-    ADD_DEFS_ALL_DOUBLE(stat);
+    // ADD_DEFS_ALL_DOUBLE(stat);
     ADD_DEFS_ALL(var);
-    ADD_DEFS_ALL_DOUBLE(var);
+    // ADD_DEFS_ALL_DOUBLE(var);
     ADD_DEFS_ALL(hist);
-    ADD_DEFS_ALL(hist_range);
+    ADD_DEFS_ALL(hist_range, const ubench_uint_t, const ubench_uint_t,
+                 const ubench_uint_t);
 
 #ifdef VERSION_INFO
     m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
